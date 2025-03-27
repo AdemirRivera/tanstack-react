b8a1c8ee609d9e0c5927e6c7aba74416ca229473
You <themrademir@gmail.com>
2025-03-27 15:54:10 -0600

    feat: agregando data inicial

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -6,6 +6,25 @@ export const useLabels = () => {
     queryKey: ["labels"],
     queryFn: getLabels,
     staleTime: 1000 * 60 * 60,
+
+    placeholderData: [
+      {
+        id: 739777675,
+        node_id: "MDU6TGFiZWw3Mzk3Nzc2NzU=",
+        url: "https://api.github.com/repos/facebook/react/labels/Component:%20Component%20API",
+        name: "Component: Component API",
+        color: "d4c5f9",
+        default: false,
+      },
+      {
+        id: 710400704,
+        node_id: "MDU6TGFiZWw3MTA0MDA3MDQ=",
+        url: "https://api.github.com/repos/facebook/react/labels/Component:%20Test%20Renderer",
+        name: "Component: Test Renderer",
+        color: "006b75",
+        default: false,
+      },
+    ],
   });
 
   return {
