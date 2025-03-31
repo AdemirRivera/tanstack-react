ce4ca1a842a4179deb59af8406a79c4bc6cd518a
You <themrademir@gmail.com>
2025-03-31 09:43:34 -0600

    feat: agregando funcionalidad a botones en el listado de issues (a nivel de diseño)

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -28,12 +28,15 @@ export const IssueItem: FC<Props> = ({ issue }) => {
   };
 
   const presetData = () => {
-    queryClient.setQueryData(["issues", issue.number], issue, {});
+    queryClient.setQueryData(["issues", issue.number], issue, {
+      updatedAt: Date.now() + 1000 * 60, // 1 minute
+    });
   };
-  
+
   return (
     <div
-      onMouseEnter={prefecthData}
+      // onMouseEnter={prefecthData}
+      onMouseEnter={presetData}
       className="animate-fadeIn flex items-center px-2 py-3 mb-5 border rounded-md bg-slate-900 hover:bg-slate-800"
     >
       {issue.state === State.Close ? (
--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { IssueList } from "../components/IssueList";
 import { LabelPicker } from "../components/LabelPicker";
 import { useIssues } from "../hooks";
 import { LoadingSpinner } from "../shared";
+import { State } from "../interfaces";
 
 export const ListView = () => {
+  const [state, setState] = useState<State>(State.All);
+
   const { issuesQuery } = useIssues();
 
   const issues = issuesQuery.data ?? [];
@@ -11,7 +15,11 @@ export const ListView = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-3 mt-5">
       <div className="col-span-1 sm:col-span-2">
-        {issuesQuery.isLoading ? <LoadingSpinner /> : <IssueList issues={issues} />}
+        {issuesQuery.isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <IssueList issues={issues} onStateChange={setState} state={state} />
+        )}
       </div>
 
       <div className="col-span-1 px-2">
